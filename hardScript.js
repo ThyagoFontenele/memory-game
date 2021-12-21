8206f940b81f59d88a60512b35d930160023141d
thyago_fontenele <thyago.fontenele@injunior.com.br>
2021-12-21 19:34:50 -0300

    responsividade finalizada

--- hardScript.js
+++ hardScript.js
@@ -2,9 +2,8 @@ var botaoStart = document.querySelector(".button");
 var win = document.querySelector('#win');
 var lose = document.querySelector('#lose');
 var blockButton = document.querySelector('.blockButtons');
-
-//win.style.display = 'none';
-//lose.style.display = 'none';
+win.style.display = 'none';
+lose.style.display = 'none';
 var botoes = document.querySelectorAll('.botao');
 var r = [];
 let n;
